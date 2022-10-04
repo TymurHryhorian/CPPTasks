5543e229f1f97f3d233f1869238d13fd17955469
TymurHryhorian <106305386+TymurHryhorian@users.noreply.github.com>
2022-10-04 12:53:44 +0300

    Update task3_13.c

--- task3_13.c
+++ task3_13.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
- 
+#include<math.h>
 int main()
 {
     const double A=4,B=6,C=13;
+    const double eps = 0.000000001;
     const double D=2,E=3,F=8;
     const double det=A*E-B*D;
-    if(det == 0){
-      if(B*F == C*E){
+    if(fabs(det) <= eps){
+      if(fabs(B*F - C*E) <= eps){
         printf("(x,y) has infinity solutions");
       } else {
         printf("(x,y) - 0 solutions");
