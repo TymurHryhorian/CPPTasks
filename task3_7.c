d087ad9b3a46f7cc77ea6e410b1a5ed3d14ab62c
TymurHryhorian <106305386+TymurHryhorian@users.noreply.github.com>
2022-10-04 12:52:07 +0300

    Update task 3.7

--- task3_7.c
+++ task3_7.c
@@ -2,62 +2,62 @@
 # include<math.h>
 
 void a (){
-    float a,b,c,r1,r2,d;
-    // не треба робити епсилон якщо флоат
+    double a,b,c,r1,r2,d,eps;
+    eps = 0.000000001;
     printf ("enter the values of a b c");
-    scanf (" %f %f %f", &a, &b, &c);
+    scanf (" %lf %lf %lf", &a, &b, &c);
     d= b*b - 4*a*c;
-    if (d>0){
+    if (d>eps){
         r1 = -b+sqrt (d) / (2*a);
         r2 = -b-sqrt (d) / (2*a);
         printf ("The real roots = %f %f", r1, r2);
     }
-    else if (d==0){
+    else if (fabs(d)<=eps){
         r1 = -b/(2*a);
         r2 = -b/(2*a);
         printf ("roots are equal =%f %f", r1, r2);
     }
-    else{
+    else if(d<-eps){
         printf("Roots are imaginary");
     }
 }
 
 
 void b (){
-    float a,b,c,r1,r2,d;
-    // не треба робити епсилон якщо флоат
+    double a,b,c,r1,r2,d,eps;
+    eps = 0.000000001;
     printf ("enter the values of a b c");
-    scanf (" %f %f %f", &a, &b, &c);
+    scanf (" %lf %lf %lf", &a, &b, &c);
     d= b*b - 4*a*c;
-    if (d>0){
+    if (d>eps){
         r1 = -b+sqrt (d) / (2*a);
         r2 = -b-sqrt (d) / (2*a);
-        if (r1 > 0){
+        if (r1 > eps){
             printf ("The real roots = %f %f\n", sqrt(r1), -sqrt(r1));
-        } else if(r1 == 0){
+        } else if(fabs(r1) <= eps){
             printf ("The real roots = %f\n", sqrt(r1));
         }
-        if (r2 > 0){
+        if (r2 > eps){
             printf ("The real roots = %f %f", sqrt(r2), -sqrt(r2));
-        } else if(r2 == 0){
+        } else if(fabs(r2) <= eps){
             printf ("The real roots = %f\n", sqrt(r2));
         }
-        if (r1 < 0 && r2 < 0){
+        if (r1 < -eps && r2 < -eps){
             printf("Roots are imaginary");
         }
     }
-    else if (d==0){
+    else if (fabs(d) <= eps){
         r1 = -b/(2*a);
         r2 = -b/(2*a);
-        if (r1 > 0){
+        if (r1 > eps){
             printf ("The real roots = %f %f\n", sqrt(r1), -sqrt(r1));
-        } else if(r1 == 0){
+        } else if(fabs(r1) <= eps){
             printf ("The real roots = %f\n", sqrt(r1));
         } else {
             printf("Roots are imaginary");
         }
     }
-    else{
+    else if (d < -eps){
         printf("Roots are imaginary");
     }
 }
